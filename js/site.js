42775a49218abf54505db7cb609bd346376ef063
Seichirino <likeanopenwindow@gmail.com>
2020-05-09 23:33:02 +1000

    Added Functionality

--- js/site.js
+++ js/site.js
@@ -1,8 +1,11 @@
 $(document).ready(function () {
 
-    $('.card').on('click', function () {
-        $('.container').toggleClass('container-origin');
-
+    $('.container').on('click', function () {
+        $(this).addClass('container-origin');
+        $(this).removeClass('pointer');
+        $(this).off('click');
+        $('.help-text').addClass('hidden');
     })
+
 
 });
