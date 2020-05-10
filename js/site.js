f657ada5242da12d2b780ce231969b96abe2eaec
Seichirino <likeanopenwindow@gmail.com>
2020-05-10 15:51:09 +1000

    Add Fucntionality

--- js/site.js
+++ js/site.js
@@ -1,10 +1,14 @@
 $(document).ready(function () {
+    if (screen.width <= 850) {
+        document.location = "mobile.html";
+    }
 
     $('.container').on('click', function () {
         $(this).addClass('container-origin');
         $(this).removeClass('pointer');
         $(this).off('click');
         $('.help-text').addClass('hidden');
+        $('a').addClass('visible');
     })
 
 
